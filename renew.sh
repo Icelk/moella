3bd0ce459e1d9cfa62724ab995747048987f0aa0
Icelk <main@icelk.dev>
2021-07-06 14:01:01 +0200

    Updated renew script

--- renew.sh
+++ renew.sh
@@ -9,6 +9,6 @@ rsync -rPhL --del "icelk@server:/etc/letsencrypt/live/" ~/.private/certs/
 # change permissions on local
 chmod -R g-r,o-r ~/.private/certs
 # sync with remote
-./sync.sh
+rsync -rLPh --del --exclude target --exclude .git . server:/home/icelk/kvarn/kvarn-reference/
 # Restart kvarn
 ssh root@server systemctl restart kvarn
