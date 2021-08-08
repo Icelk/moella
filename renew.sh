02778d8c1fd614473db05fc2522ca34065370908
Icelk <main@icelk.dev>
2021-08-08 16:56:03 +0200

    Fixed major issue in renew script.

--- renew.sh
+++ renew.sh
@@ -3,7 +3,7 @@
 # Here, I use the username `icelk` and have `server` configured the target IP in `/etc/hosts`
 
 # Renew and change permissions
-ssh root@server certbot renew && chown icelk:icelk -R /etc/letsencrypt && chmod o-r,g-r -R /etc/letsencrypt
+ssh root@server "certbot renew && chown icelk:icelk -R /etc/letsencrypt && chmod o-r,g-r -R /etc/letsencrypt"
 # Pull to local
 rsync -rPhL --del icelk@server:/etc/letsencrypt/live/ ~/.private/certs/
 # change permissions on local
