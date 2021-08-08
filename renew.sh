beec1838837436e3753b4f1114efc160e7d6e765
Icelk <main@icelk.dev>
2021-08-08 16:46:30 +0200

    Graceful shutdown is now included in production feature set.

--- renew.sh
+++ renew.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
-# Here, I use the username `icelk` and have `server` configures to a IP in `/etc/hosts`
+# Here, I use the username `icelk` and have `server` configured the target IP in `/etc/hosts`
 
 # Renew and change permissions
 ssh root@server certbot renew && chown icelk:icelk -R /etc/letsencrypt && chmod o-r,g-r -R /etc/letsencrypt
 # Pull to local
-rsync -rPhL --del "icelk@server:/etc/letsencrypt/live/" ~/.private/certs/
+rsync -rPhL --del icelk@server:/etc/letsencrypt/live/ ~/.private/certs/
 # change permissions on local
 chmod -R g-r,o-r ~/.private/certs
 # sync with remote
