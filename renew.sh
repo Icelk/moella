e99f8dd676c7ba82e6bdab8559decbc40617abc3
Icelk <main@icelk.dev>
2022-10-23 14:34:39 +0200

    Updated renew script

--- renew.sh
+++ renew.sh
@@ -11,7 +11,8 @@ rsync -rPhL --del icelk@server:/etc/letsencrypt/live/ ~/.private/certs/
 # change permissions on local
 chmod -R g-r,o-r ~/.private/certs
 # sync with remote
-rsync -rLPh --exclude target --exclude .git . server:/home/icelk/kvarn/kvarn-reference/
+rsync -rLPh --exclude target --exclude .git . server:~/kvarn/kvarn-reference/
+# refresh email certs
+ssh root@server "/root/scripts/sin-dovecot-postfix.sh"
 # Restart kvarn
 ssh root@server "systemctl reload kvarn; systemctl restart unbound postfix"
-ssh root@server "/root/scripts/sin-dovecot-postfix.sh"
