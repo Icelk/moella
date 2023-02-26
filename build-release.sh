37ebd8f5b82083b3b82ee9ac22cb272d24c620fd
Icelk <main@icelk.dev>
2023-02-26 01:13:50 +0100

    Improve build script, and support windows in it.

--- build-release.sh
+++ build-release.sh
@@ -12,11 +12,17 @@ cb() {
 cb_mac() {
     CC=o64-clang cb --target x86_64-apple-darwin $@
 }
+cb_win() {
+    cb --target x86_64-pc-windows-gnu $@
+}
 
-cb_all() {
+cb_unix() {
     cb $@ &
     cb_mac $@ &
-    wait
+}
+cb_all() {
+    cb_unix $@ &
+    cb_win $@ &
 }
 
 wd=$PWD
@@ -24,21 +30,30 @@ wd=$PWD
 cb_all -F bin --bin moella &
 
 cd ../kvarn/ctl
-cb_all &
+cb_mac --no-default-features &
+cb &
 
 cd ../chute
-cb_all &
+cb_mac --no-default-features -F date,bin &
+cb_win &
+cb &
 
 wait
 
 cd $wd
+cp ./target/x86_64-pc-windows-gnu/distribution/moella.exe $wd/
 cp ./target/x86_64-apple-darwin/distribution/moella moella-macos
+# we don't need to strip the win binaries (cargo actually handles that)
+x86_64-apple-darwin14-strip $wd/moella-macos
 cp ./target/distribution/moella moella-linux
 
 cd ../kvarn/ctl
 cp ../target/x86_64-apple-darwin/distribution/kvarnctl $wd/kvarnctl-macos
+x86_64-apple-darwin14-strip $wd/kvarnctl-macos
 cp ../target/distribution/kvarnctl $wd/kvarnctl-linux
 
 cd ../chute
+cp ../target/x86_64-pc-windows-gnu/distribution/chute.exe $wd/
 cp ../target/x86_64-apple-darwin/distribution/chute $wd/chute-macos
+x86_64-apple-darwin14-strip $wd/chute-macos
 cp ../target/distribution/chute $wd/chute-linux
